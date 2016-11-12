16ec12d8b5d820e2ffa76aa4024333121e1d3214
Koby Picker <jkp46@case.edu>
2016-11-12 15:17:54 -0500

    First commit
    Signed-off-by: Koby Picker <jkp46@case.edu>

---
+++ sandbox.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jkobyp/shapes"
+)
+
+func main() {
+	// Variable assignment
+	var width int = 400
+	height := 600
+
+	// Calling package functions
+	w := shapes.InitWindow(width, height)
+	fmt.Printf("hello, world")
+
+	// Creating objects
+	p := shapes.Point{5, 5}
+	// Create a Circle called "c" with radius 10
+
+	// Method calls
+	c.Move(p)
+	// Move the Circle to 100, 100
+
+	// Use the documentation to add the circle to our Window w
+	//
+
+	// go to tour.golang.org !
+}
