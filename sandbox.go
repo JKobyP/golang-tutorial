91ebde5275db5d45b22452428c33ed0497bea37e
Koby Picker <jkp46@case.edu>
2016-11-13 14:54:11 -0500

    Explores more concepts
    Signed-off-by: Koby Picker <jkp46@case.edu>

--- sandbox.go
+++ sandbox.go
@@ -6,24 +6,38 @@ import (
 )
 
 func main() {
-	// Variable assignment
+	// ***Variable assignment
 	var width int = 400
 	height := 600
 
-	// Calling package functions
+	// ***Calling package functions
 	w := shapes.InitWindow(width, height)
 	fmt.Printf("hello, world")
 
-	// Creating objects
+	// ***Creating objects
 	p := shapes.Point{5, 5}
 	// Create a Circle called "c" with radius 10
 
-	// Method calls
+	// ***Method calls
 	c.Move(p)
 	// Move the Circle to 100, 100
 
 	// Use the documentation to add the circle to our Window w
 	//
 
+	// ***Loops
+	for w.Area.Includes(c.Location()) {
+		// Animate the circle's motion
+	}
 	// go to tour.golang.org !
+}
+
+//***Struct declaration
+type Triangle struct {
+	// Give it fields
+}
+
+//***Method declaration
+func (t Triangle) Location() shapes.Point {
+	// Implement me, then see if you can add me to "w"
 }
